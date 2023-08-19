679f057eb0c25c92e8fe275f7e7dfb49d020a789
Mikko Ankkala <mikko.kumara@gmail.com>
2023-08-19 05:51:27 +0300

    Rename accessed_block to accessed_chunk

--- terrain/voxel_mesh_map.h
+++ terrain/voxel_mesh_map.h
@@ -13,20 +13,20 @@ namespace zylann::voxel {
 template <typename ChunkMesh_T>
 class VoxelMeshMap {
 public:
-	VoxelMeshMap() : _last_accessed_block(nullptr) {}
+	VoxelMeshMap() : _last_accessed_chunk(nullptr) {}
 
 	~VoxelMeshMap() {
 		clear();
 	}
 
 	struct NoAction {
-		inline void operator()(ChunkMesh_T &block) {}
+		inline void operator()(ChunkMesh_T &chunk) {}
 	};
 
 	template <typename Action_T>
 	void remove_chunk(Vector3i bpos, Action_T pre_delete) {
-		if (_last_accessed_block && _last_accessed_block->position == bpos) {
-			_last_accessed_block = nullptr;
+		if (_last_accessed_chunk && _last_accessed_chunk->position == bpos) {
+			_last_accessed_chunk = nullptr;
 		}
 		auto it = _chunks_map.find(bpos);
 		if (it != _chunks_map.end()) {
@@ -34,65 +34,65 @@ public:
 #ifdef DEBUG_ENABLED
 			CRASH_COND(i >= _chunks.size());
 #endif
-			ChunkMesh_T *block = _chunks[i];
-			ERR_FAIL_COND(block == nullptr);
-			pre_delete(*block);
-			queue_free_chunk_mesh(block);
+			ChunkMesh_T *chunk = _chunks[i];
+			ERR_FAIL_COND(chunk == nullptr);
+			pre_delete(*chunk);
+			queue_free_chunk_mesh(chunk);
 			remove_chunk_internal(it, i);
 		}
 	}
 
 	ChunkMesh_T *get_chunk(Vector3i bpos) {
-		if (_last_accessed_block && _last_accessed_block->position == bpos) {
-			return _last_accessed_block;
+		if (_last_accessed_chunk && _last_accessed_chunk->position == bpos) {
+			return _last_accessed_chunk;
 		}
 		auto it = _chunks_map.find(bpos);
 		if (it != _chunks_map.end()) {
 #ifdef DEBUG_ENABLED
 			const unsigned int i = it->second.index;
 			CRASH_COND(i >= _chunks.size());
-			ChunkMesh_T *block = _chunks[i];
-			CRASH_COND(block == nullptr); // The map should not contain null chunks
-			CRASH_COND(it->second.block == nullptr);
+			ChunkMesh_T *chunk = _chunks[i];
+			CRASH_COND(chunk == nullptr); // The map should not contain null chunks
+			CRASH_COND(it->second.chunk == nullptr);
 #endif
-			_last_accessed_block = it->second.block;
-			return _last_accessed_block;
+			_last_accessed_chunk = it->second.chunk;
+			return _last_accessed_chunk;
 		}
 		return nullptr;
 	}
 
 	const ChunkMesh_T *get_chunk(Vector3i bpos) const {
-		if (_last_accessed_block != nullptr && _last_accessed_block->position == bpos) {
-			return _last_accessed_block;
+		if (_last_accessed_chunk != nullptr && _last_accessed_chunk->position == bpos) {
+			return _last_accessed_chunk;
 		}
 		auto it = _chunks_map.find(bpos);
 		if (it != _chunks_map.end()) {
 #ifdef DEBUG_ENABLED
 			const unsigned int i = it->second.index;
 			CRASH_COND(i >= _chunks.size());
-			ChunkMesh_T *block = _chunks[i];
-			CRASH_COND(block == nullptr); // The map should not contain null chunks
-			CRASH_COND(it->second.block == nullptr);
+			ChunkMesh_T *chunk = _chunks[i];
+			CRASH_COND(chunk == nullptr); // The map should not contain null chunks
+			CRASH_COND(it->second.chunk == nullptr);
 #endif
 			// This function can't cache _last_accessed_chunk, because it's const, so repeated accesses are hashing
 			// again...
-			return it->second.block;
+			return it->second.chunk;
 		}
 		return nullptr;
 	}
 
-	void set_chunk(Vector3i bpos, ChunkMesh_T *block) {
-		ERR_FAIL_COND(block == nullptr);
-		CRASH_COND(bpos != block->position);
-		if (_last_accessed_block == nullptr || _last_accessed_block->position == bpos) {
-			_last_accessed_block = block;
+	void set_chunk(Vector3i bpos, ChunkMesh_T *chunk) {
+		ERR_FAIL_COND(chunk == nullptr);
+		CRASH_COND(bpos != chunk->position);
+		if (_last_accessed_chunk == nullptr || _last_accessed_chunk->position == bpos) {
+			_last_accessed_chunk = chunk;
 		}
 #ifdef DEBUG_ENABLED
 		CRASH_COND(has_chunk(bpos));
 #endif
 		unsigned int i = _chunks.size();
-		_chunks.push_back(block);
-		_chunks_map.insert({ bpos, { block, i } });
+		_chunks.push_back(chunk);
+		_chunks_map.insert({ bpos, { chunk, i } });
 	}
 
 	bool has_chunk(Vector3i pos) const {
@@ -102,16 +102,16 @@ public:
 
 	void clear() {
 		for (auto it = _chunks.begin(); it != _chunks.end(); ++it) {
-			ChunkMesh_T *block = *it;
-			if (block == nullptr) {
+			ChunkMesh_T *chunk = *it;
+			if (chunk == nullptr) {
 				ERR_PRINT("Unexpected nullptr in VoxelMap::clear()");
 			} else {
-				memdelete(block);
+				memdelete(chunk);
 			}
 		}
 		_chunks.clear();
 		_chunks_map.clear();
-		_last_accessed_block = nullptr;
+		_last_accessed_chunk = nullptr;
 	}
 
 	unsigned int get_chunk_count() const {
@@ -125,28 +125,28 @@ public:
 	template <typename Op_T>
 	inline void for_each_chunk(Op_T op) {
 		for (auto it = _chunks.begin(); it != _chunks.end(); ++it) {
-			ChunkMesh_T *block = *it;
+			ChunkMesh_T *chunk = *it;
 #ifdef DEBUG_ENABLED
-			CRASH_COND(block == nullptr);
+			CRASH_COND(chunk == nullptr);
 #endif
-			op(*block);
+			op(*chunk);
 		}
 	}
 
 	template <typename Op_T>
 	inline void for_each_chunk(Op_T op) const {
 		for (auto it = _chunks.begin(); it != _chunks.end(); ++it) {
-			const ChunkMesh_T *block = *it;
+			const ChunkMesh_T *chunk = *it;
 #ifdef DEBUG_ENABLED
-			CRASH_COND(block == nullptr);
+			CRASH_COND(chunk == nullptr);
 #endif
-			op(*block);
+			op(*chunk);
 		}
 	}
 
 private:
 	struct MapItem {
-		ChunkMesh_T *block;
+		ChunkMesh_T *chunk;
 		// Index of the chunk within the vector storage
 		unsigned int index;
 	};
@@ -159,32 +159,32 @@ private:
 		// This function assumes the chunk is already freed
 		_chunks_map.erase(rm_it);
 
-		ChunkMesh_T *moved_block = _chunks.back();
+		ChunkMesh_T *moved_chunk = _chunks.back();
 #ifdef DEBUG_ENABLED
 		CRASH_COND(index >= _chunks.size());
 #endif
-		_chunks[index] = moved_block;
+		_chunks[index] = moved_chunk;
 		_chunks.pop_back();
 
 		if (index < _chunks.size()) {
-			auto moved_chunk_index_it = _chunks_map.find(moved_block->position);
+			auto moved_chunk_index_it = _chunks_map.find(moved_chunk->position);
 			CRASH_COND(moved_chunk_index_it == _chunks_map.end());
 			moved_chunk_index_it->second.index = index;
 		}
 	}
 
-	static void queue_free_chunk_mesh(ChunkMesh_T *block) {
+	static void queue_free_chunk_mesh(ChunkMesh_T *chunk) {
 		// We spread this out because of physics
 		// TODO Could it be enough to do both render and physic deallocation with the task in ~ChunkMesh_T()?
 		struct FreeChunkMeshTask : public zylann::ITimeSpreadTask {
 			void run(TimeSpreadTaskContext &ctx) override {
-				memdelete(block);
+				memdelete(chunk);
 			}
-			ChunkMesh_T *block = nullptr;
+			ChunkMesh_T *chunk = nullptr;
 		};
-		ERR_FAIL_COND(block == nullptr);
+		ERR_FAIL_COND(chunk == nullptr);
 		FreeChunkMeshTask *task = memnew(FreeChunkMeshTask);
-		task->block = block;
+		task->chunk = chunk;
 		VoxelEngine::get_singleton().push_main_thread_time_spread_task(task);
 	}
 
@@ -197,7 +197,7 @@ private:
 
 	// Voxel access will most frequently be in contiguous areas, so the same chunks are accessed.
 	// To prevent too much hashing, this reference is checked before.
-	mutable ChunkMesh_T *_last_accessed_block;
+	mutable ChunkMesh_T *_last_accessed_chunk;
 };
 
 } // namespace zylann::voxel
